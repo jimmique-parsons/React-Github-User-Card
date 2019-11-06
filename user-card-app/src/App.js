8e94214aa9f3fc03bc0dddabc46522063ef1930a
Jimmique <jimmique88@gmail.com>
2019-11-05 19:12:55 -0800

    Fetched Follower data

--- user-card-app/src/App.js
+++ user-card-app/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 class App extends React.Component {
 
   state = {
-    userData: {}
+    userData: {},
+    userFollowers: []
   };
 
   componentDidMount() {
@@ -12,26 +13,49 @@ class App extends React.Component {
       .then( res => res.json())
       .then( data => this.setState({ userData: data }))
       .catch( err => console.log(err));
+
+    fetch('https://api.github.com/users/jimmique-parsons/followers')
+      .then( res => res.json())
+      .then( data => this.setState({ userFollowers: data }))
+      .catch( err => console.log(err));
   };
 
   render() {
     return (
       <div className="App">
-        <header>
+        
           <h1>Github User Cards</h1>
           <div className="container">
-            <div className="card">
-              <img 
-                className="card__avatar"
-                alt={this.state.userData.login}
-                src={this.state.userData.avatar_url}
-              />
-               <h4>{this.state.userData.login}</h4>
-               <p>Bio: {this.state.userData.bio}</p>
-               <p>Followers: {this.state.userData.followers}</p>
-            </div>
+             <a href={this.state.userData.html_url} className="card user-card">
+               <div>
+                 <img 
+                   className="card__avatar"
+                   alt={this.state.userData.login}
+                   src={this.state.userData.avatar_url}
+                 />
+                 <h4>{this.state.userData.login}</h4>
+                 <p>Bio: {this.state.userData.bio}</p>
+                 <p>Followers: {this.state.userData.followers}</p>
+               </div>
+             </a>
+           </div>
+           <h2>Followers</h2>
+          <div className="container">
+          {this.state.userFollowers.map( follower => {
+               return (
+                 <a href={follower.html_url} className="card">
+                   <div>
+                     <img 
+                       className="card__avatar"
+                       alt={follower.login}
+                       src={follower.avatar_url}
+                     />
+                     <h4>{follower.login}</h4>
+                   </div>
+                 </a>
+               );
+             })}
           </div>
-        </header>
       </div>
     );
  };
