3db582e7cf27fc432ac06e2b9553b40eb94d8a48
Jimmique <jimmique88@gmail.com>
2019-11-05 18:50:44 -0800

    Converted to Class component and fetched and displayed user data

--- user-card-app/src/App.js
+++ user-card-app/src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Github User Cards</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    userData: {}
+  };
+
+  componentDidMount() {
+    fetch('https://api.github.com/users/jimmique-parsons')
+      .then( res => res.json())
+      .then( data => this.setState({ userData: data }))
+      .catch( err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Github User Cards</h1>
+          <div className="container">
+            <div className="card">
+              <h4>{this.state.userData.login}</h4>
+              <img 
+                className="card__avatar"
+                alt={this.state.userData.login}
+                src={this.state.userData.avatar_url}
+              />
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+ };
 }
 
 export default App;
